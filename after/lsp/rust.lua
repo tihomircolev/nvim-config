e3695c3c7d81d4f5abcaa6b514ad4e6fe241aa04
tihomircolev <tihomircolev@gmail.com>
2025-07-18 14:25:44 +0300

    add rust to lsp

---
+++ after/lsp/rust.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/after/lsp/rust.lua
+return {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
+}
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -119,11 +119,11 @@ vim.lsp.config("*", {
 
 -- A mapping from lsp server name to the executable name
 local enabled_lsp_servers = {
-  -- pyright = "delance-langserver",
+  pyright = "delance-langserver",
   ruff = "ruff",
   lua_ls = "lua-language-server",
-  -- ltex = "ltex-ls",
-  -- clangd = "clangd",
+  rust_analyzer = "rust-analyzer",
+  clangd = "clangd",
   vimls = "vim-language-server",
   bashls = "bash-language-server",
   yamlls = "yaml-language-server",
